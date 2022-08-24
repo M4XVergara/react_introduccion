100eb36422e041f31c9676651007d36d941d52a0
M4XVergara <jakerax@gmail.com>
2022-08-24 09:56:30 -0400

    first commit

---
+++ src/cards.js
@@ -0,0 +1,10 @@
+import './cards.css'
+
+export default function cards() {
+    return (
+        <div className="box">
+            <div className='card-left'></div>
+            <div className='card-right'></div>
+        </div>
+    )
+}
